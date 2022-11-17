87ade524b544e33dd9ccdb7c76afc8f8bdd13354
Albert Chang <a89529294@hotmail.com>
2022-11-17 16:28:17 +0800

    feat: remove logos from images in automation

--- src/components/automation/ImageSwitcher.tsx
+++ src/components/automation/ImageSwitcher.tsx
@@ -2,10 +2,10 @@ import cx from "classix";
 import React, { useState } from "react";
 import Image from "next/image";
 
-import switch1 from "src/assets/automation/switch1.webp";
-import switch2 from "src/assets/automation/switch2.webp";
-import switch3 from "src/assets/automation/switch3.webp";
-import switch4 from "src/assets/automation/switch4.webp";
+import switch1 from "src/assets/automation/switch1-new.webp";
+import switch2 from "src/assets/automation/switch2-new.webp";
+import switch3 from "src/assets/automation/switch3-new.webp";
+import switch4 from "src/assets/automation/switch4-new.webp";
 
 function ImageSwitcher() {
   const [selectedIdx, selectIdx] = useState(0);
@@ -28,8 +28,11 @@ function ImageSwitcher() {
         </ul>
       </nav>
       <div className="flex flex-1 justify-center bg-cerebral-gray">
-        <div className="h-full bg-white">
+        <div className="relative h-full bg-white">
           <Image src={images[selectedIdx]} alt="" className="h-full w-auto" />
+          <span className="absolute bottom-8 left-1/2 -translate-x-1/2 text-3xl font-bold">
+            {captions[selectedIdx]}
+          </span>
         </div>
       </div>
     </div>
@@ -37,6 +40,7 @@ function ImageSwitcher() {
 }
 
 const tabs = ["產線圖 - Ｉ型", "產線圖 - Ｕ型", "產線圖 - Ｙ型", "產線圖 - Ｚ型"];
+const captions = ["Ｉ型自動化產線", "Ｕ型自動化產線", "Ｙ型自動化產線", "Ｚ型自動化產線"];
 const images = [switch1, switch2, switch3, switch4];
 
 export default ImageSwitcher;
